8cecda77953bb65a7c431885cc80d94c185dca5f
Jack Soderstrom <JaxSoder@gmail.com>
2021-07-21 16:15:50 -0500

    Refactored multiple methods

--- src/visual.ts
+++ src/visual.ts
@@ -72,7 +72,7 @@ interface AnnotatedBarSettings {
 }
 
 //Individual values and settings from specific data point
-interface AnnotatedBarDataPoint {
+export interface AnnotatedBarDataPoint {
   colName: string;
   colVal: string;
   value: any;
@@ -94,7 +94,7 @@ interface AnnotatedBarDataPoint {
   highlight: boolean
 }
 
-interface AnnotatedBarViewModel {
+export interface AnnotatedBarViewModel {
   dataPoints: AnnotatedBarDataPoint[];
   settings: AnnotatedBarSettings
 }
@@ -304,22 +304,159 @@ export class Visual implements IVisual {
     this.events = options.host.eventService;
   }
 
-  /*
-  When a value is changed in the Format Section of a graph on Power Bi enumerateObjectInstances() is called.
-  */
-  public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstanceEnumeration {
-    console.log("Something is changed");
-    return;
-  }
 
-  private validateData(data: AnnotatedBarDataPoint[], options: VisualUpdateOptions) {
-    if (data.length > 10000) {
-        return true;
-    }
-    return false;
+/**When a value is changed in the Format Section of a graph on Power Bi enumerateObjectInstances() is called.*/
+public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstanceEnumeration 
+{
+  //Push custom attributes to property pane, as well as dynamic names.
+  let objectName = options.objectName;
+  console.log("enumerateObjectInstancesMAIN OBJECT NAME : " + options.objectName);
+  let objectEnumeration: VisualObjectInstance[] = [];
+  let dataPoints = this.viewModel.dataPoints.concat()
+
+  switch (objectName) 
+  {
+    case 'annotationSettings':
+      this.annotationSettingsUpdateWhenFormatIsChanged(objectName, dataPoints, objectEnumeration);
+      break;
+
+    case 'textFormatting':
+      this.textFormattingUpdateWhenFormatIsChanged(objectName, dataPoints, objectEnumeration);
+      break;
+
+    case 'axisSettings':
+      this.axisSettingsUpdateWhenFormatIsChanged(objectName, dataPoints, objectEnumeration);
+      break;
+
+    case 'barColorSelector':
+      this.barColorSelectorUpdateWhenFormatIsChanged(objectName, dataPoints, objectEnumeration);
+      break;
+  };
+
+  return objectEnumeration;
 }
 
+/** This Updates the "Style" section on the format menu */
+private annotationSettingsUpdateWhenFormatIsChanged(objectName: string, dataPoints: AnnotatedBarDataPoint[], objectEnumeration: VisualObjectInstance[])
+{
 
+  
+  console.log("running");
+  objectEnumeration.push({
+    objectName: objectName,
+    properties: {
+      overlapStyle: this.viewModel.settings.annotationSettings.overlapStyle,
+      labelInfo: this.viewModel.settings.annotationSettings.labelInfo,
+      barHt: this.viewModel.settings.annotationSettings.barHt,
+      sameAsBarColor: this.viewModel.settings.annotationSettings.sameAsBarColor,
+      hideLabels: this.viewModel.settings.annotationSettings.hideLabels,
+      displayUnits: this.viewModel.settings.annotationSettings.displayUnits,
+      precision: this.viewModel.settings.annotationSettings.precision,
+      stagger: this.viewModel.settings.annotationSettings.stagger,
+    },selector: null});
+  if (this.viewModel.settings.annotationSettings.labelInfo === 'Auto') {
+    objectEnumeration.push({
+      objectName: objectName,
+      properties: { separator: this.viewModel.settings.annotationSettings.separator, },selector: null});}
+}
+
+/** This Updates the "Label Formatting" section on the format menu */
+private textFormattingUpdateWhenFormatIsChanged(objectName: string, dataPoints: AnnotatedBarDataPoint[], objectEnumeration: VisualObjectInstance[])
+{
+objectEnumeration.push({
+  objectName: objectName,
+  properties: {allTextTop: this.viewModel.settings.textFormatting.allTextTop,
+  },selector: null})
+
+if (this.viewModel.settings.annotationSettings.hideLabels) {
+  objectEnumeration.push({
+    objectName: objectName,
+    properties: {fill: {solid: {color: this.viewModel.settings.textFormatting.fill}}},selector: null})}
+
+if (!this.viewModel.settings.annotationSettings.sameAsBarColor) {
+  objectEnumeration.push({
+    objectName: objectName,
+    properties: {fill: {solid: {color: false}}},selector: null})
+  }
+objectEnumeration.push({
+  objectName: objectName,properties: {
+    labelOrientation: this.viewModel.settings.textFormatting.labelOrientation,
+    annotationStyle: this.viewModel.settings.textFormatting.annotationStyle,
+    FontFamily: this.viewModel.settings.textFormatting.FontFamily,
+    fontSize: this.viewModel.settings.textFormatting.fontSize
+  },selector: null})
+for (let barDataPoint of dataPoints){//.sort((a, b) => (a.value > b.value) ? 1 : -1)) {
+  objectEnumeration.push({
+    objectName: objectName, displayName: barDataPoint.displayName + " custom format",
+    properties: {customFormat: barDataPoint.customFormat},selector: barDataPoint.selectionId.getSelector()});
+  if (barDataPoint.customFormat) {
+    if (!this.viewModel.settings.annotationSettings.sameAsBarColor) {
+      objectEnumeration.push({
+        objectName: objectName, displayName: barDataPoint.displayName + " Color",
+        properties: {fill: {solid: {color: barDataPoint.LabelColor}}
+        },selector: barDataPoint.selectionId.getSelector()});
+    }
+    objectEnumeration.push({
+      objectName: objectName, displayName: barDataPoint.displayName + " text position",
+      properties: {top: barDataPoint.top
+      },selector: barDataPoint.selectionId.getSelector()});
+    objectEnumeration.push({
+      objectName: barDataPoint.displayName, displayName: barDataPoint.displayName,
+      properties: {
+        FontFamily: barDataPoint.FontFamily, fontSize: barDataPoint.fontSize},selector: barDataPoint.selectionId.getSelector()});
+    objectEnumeration.push({
+      objectName: objectName, displayName: barDataPoint.displayName + " Label orientation",
+      properties: { labelOrientation: barDataPoint.labelOrientation },selector: barDataPoint.selectionId.getSelector()}); }
+  }
+}
+
+/**This Updates the "Xaxis" section on the format menu*/
+private axisSettingsUpdateWhenFormatIsChanged(objectName: string, dataPoints: AnnotatedBarDataPoint[], objectEnumeration: VisualObjectInstance[])
+{
+  objectEnumeration.push({
+    objectName: objectName, properties: {
+      axis: this.viewModel.settings.axisSettings.axis,
+      axisColor: this.viewModel.settings.axisSettings.axisColor
+    },selector: null});
+  if (this.viewModel.settings.axisSettings.axis !== "None") {
+    objectEnumeration.push({
+      objectName: objectName, properties: {
+        fontSize: this.viewModel.settings.axisSettings.fontSize,
+        fontFamily: this.viewModel.settings.axisSettings.fontFamily,
+        bold: this.viewModel.settings.axisSettings.bold
+      },selector: null});}
+  if (this.viewModel.settings.annotationSettings.overlapStyle !== "stacked") {
+    objectEnumeration.push({
+      objectName: objectName, properties: {manualScale: this.viewModel.settings.axisSettings.manualScale,
+      },selector: null});
+    if (this.viewModel.settings.axisSettings.manualScale) {
+      objectEnumeration.push({
+        objectName: objectName, properties: {
+          barMin: this.viewModel.settings.axisSettings.barMin, barMax: this.viewModel.settings.axisSettings.barMax,
+        },selector: null});}}
+}
+
+/**This Updates the "Bar formatting" section on the format menu*/
+private barColorSelectorUpdateWhenFormatIsChanged(objectName: string, dataPoints: AnnotatedBarDataPoint[], objectEnumeration: VisualObjectInstance[])
+{
+  for (let barDataPoint of dataPoints){//.sort((a, b) => (a.value > b.value) ? 1 : -1)) {
+    objectEnumeration.push({
+      objectName: objectName, displayName: "Display " + barDataPoint.displayName + " in bar",
+      properties: {ShowInBar: barDataPoint.ShowInBar
+      },selector: barDataPoint.selectionId.getSelector()});
+    if (barDataPoint.ShowInBar) {
+      objectEnumeration.push({
+        objectName: objectName, displayName: barDataPoint.displayName,
+        properties: {fill: {solid: {color: barDataPoint.barColor}} },selector: barDataPoint.selectionId.getSelector()});}}
+}
+
+private validateData(data: AnnotatedBarDataPoint[], options: VisualUpdateOptions) {
+  if (data.length > 10000) {
+      return true;
+  }
+  return false;
+  
+}
 
   public update(options) {
     this.events.renderingStarted(options); // Rendering Events API START
